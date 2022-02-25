dddfc48e564550d0da063a12fcb4e838c272bcab
TeamLider9141 <juraqulovortiq2002@gmail.com>
2022-02-26 00:02:41 +0500

    Finished What's the hellgit status!

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace trig_functions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picGraph = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtXmin = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtXmax = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtYmax = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtYmin = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnGraph = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picGraph
+            // 
+            this.picGraph.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.picGraph.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picGraph.Location = new System.Drawing.Point(12, 64);
+            this.picGraph.Name = "picGraph";
+            this.picGraph.Size = new System.Drawing.Size(656, 188);
+            this.picGraph.TabIndex = 0;
+            this.picGraph.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X Min:";
+            // 
+            // txtXmin
+            // 
+            this.txtXmin.Location = new System.Drawing.Point(54, 12);
+            this.txtXmin.Name = "txtXmin";
+            this.txtXmin.Size = new System.Drawing.Size(42, 20);
+            this.txtXmin.TabIndex = 2;
+            this.txtXmin.Text = "-1.1";
+            this.txtXmin.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label3.Location = new System.Drawing.Point(95, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "*p";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label2.Location = new System.Drawing.Point(95, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "*p";
+            // 
+            // txtXmax
+            // 
+            this.txtXmax.Location = new System.Drawing.Point(54, 38);
+            this.txtXmax.Name = "txtXmax";
+            this.txtXmax.Size = new System.Drawing.Size(42, 20);
+            this.txtXmax.TabIndex = 6;
+            this.txtXmax.Text = "3.1";
+            this.txtXmax.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "X Max:";
+            // 
+            // txtYmax
+            // 
+            this.txtYmax.Location = new System.Drawing.Point(196, 38);
+            this.txtYmax.Name = "txtYmax";
+            this.txtYmax.Size = new System.Drawing.Size(42, 20);
+            this.txtYmax.TabIndex = 12;
+            this.txtYmax.Text = "2.1";
+            this.txtYmax.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(154, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Y Max:";
+            // 
+            // txtYmin
+            // 
+            this.txtYmin.Location = new System.Drawing.Point(196, 12);
+            this.txtYmin.Name = "txtYmin";
+            this.txtYmin.Size = new System.Drawing.Size(42, 20);
+            this.txtYmin.TabIndex = 9;
+            this.txtYmin.Text = "-2.1";
+            this.txtYmin.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(154, 15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Y Min:";
+            // 
+            // btnGraph
+            // 
+            this.btnGraph.Location = new System.Drawing.Point(258, 24);
+            this.btnGraph.Name = "btnGraph";
+            this.btnGraph.Size = new System.Drawing.Size(75, 23);
+            this.btnGraph.TabIndex = 13;
+            this.btnGraph.Text = "Graph";
+            this.btnGraph.UseVisualStyleBackColor = true;
+            this.btnGraph.Click += new System.EventHandler(this.btnGraph_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(352, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Sine";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Green;
+            this.label7.Location = new System.Drawing.Point(352, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Cosine";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.Blue;
+            this.label9.Location = new System.Drawing.Point(352, 41);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Tangent";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGraph;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 264);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnGraph);
+            this.Controls.Add(this.txtYmax);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtYmin);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtXmax);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtXmin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picGraph);
+            this.Name = "Form1";
+            this.Text = "howto_graph_trig_functions";
+            ((System.ComponentModel.ISupportInitialize)(this.picGraph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picGraph;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtXmin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtXmax;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtYmax;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtYmin;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnGraph;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Drawing.Drawing2D;
+
+namespace trig_functions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // The image used for the graph.
+        private Bitmap GraphImage;
+
+        // Graph.
+        private void btnGraph_Click(object sender, EventArgs e)
+        {
+            GraphImage = new Bitmap(
+                picGraph.ClientSize.Width,
+                picGraph.ClientSize.Height);
+            using (Graphics gr = Graphics.FromImage(GraphImage))
+            {
+                gr.Clear(Color.White);
+                gr.SmoothingMode = SmoothingMode.AntiAlias;
+
+                using (Pen thin_pen = new Pen(Color.Purple, 0))
+                {
+                    // Get the bounds.
+                    double xmin = double.Parse(txtXmin.Text) * Math.PI;
+                    double xmax = double.Parse(txtXmax.Text) * Math.PI;
+                    double ymin = double.Parse(txtYmin.Text);
+                    double ymax = double.Parse(txtYmax.Text);
+
+                    // Scale to make the area fit the PictureBox.
+                    RectangleF world_coords = new RectangleF(
+                        (float)xmin, (float)ymax,
+                        (float)(xmax - xmin),
+                        (float)(ymin - ymax));
+                    PointF[] device_coords =
+                    {
+                        new PointF(0, 0),
+                        new PointF(picGraph.ClientSize.Width, 0),
+                        new PointF(0, picGraph.ClientSize.Height),
+                    };
+                    gr.Transform = new Matrix(world_coords, device_coords);
+
+                    // Draw the X-axis.
+                    // Start at the multiple of Pi < xmin.
+                    double start_x = Math.PI * ((int)(xmin - 1));
+                    gr.DrawLine(thin_pen, (float)xmin, 0, (float)xmax, 0);
+                    float dy = (float)((ymax - ymin) / 30.0);
+                    for (double x = start_x; x <= xmax; x += Math.PI)
+                    {
+                        gr.DrawLine(thin_pen, (float)x, -2 * dy, (float)x, 2 * dy);
+                    }
+                    for (double x = start_x + Math.PI / 2.0; x <= xmax; x += Math.PI)
+                    {
+                        gr.DrawLine(thin_pen, (float)x, -dy, (float)x, dy);
+                    }
+
+                    // Draw the Y-axis.
+                    // Start at the multiple of 1 < ymin.
+                    double start_y = (int)ymin - 1;
+                    gr.DrawLine(thin_pen, 0, (float)ymin, 0, (float)ymax);
+                    float dx = (float)((xmax - xmin) / 60.0);
+                    for (double y = start_y; y <= ymax; y += 1.0)
+                    {
+                        gr.DrawLine(thin_pen, -2 * dx, (float)y, 2 * dx, (float)y);
+                    }
+                    for (double y = start_y + 0.5; y <= ymax; y += 1.0)
+                    {
+                        gr.DrawLine(thin_pen, -dx, (float)y, dx, (float)y);
+                    }
+
+                    // Draw vertical asymptotes.
+                    thin_pen.DashPattern = new float[] { 5, 5 };
+                    for (double x = start_x + Math.PI / 2.0; x <= xmax; x += Math.PI)
+                    {
+                        gr.DrawLine(thin_pen, (float)x, (float)ymin, (float)x, (float)ymax);
+                    }
+
+                    // Draw horizontal limits for sine and cosine.
+                    gr.DrawLine(thin_pen, (float)xmin, 1, (float)xmax, 1);
+                    gr.DrawLine(thin_pen, (float)xmin, -1, (float)xmax, -1);
+                    thin_pen.DashStyle = DashStyle.Solid;
+                    
+                    // See how big a pixel is before scaling.
+                    Matrix inverse = gr.Transform;
+                    inverse.Invert();
+                    PointF[] pixel_pts =
+                    {
+                        new PointF(0, 0),
+                        new PointF(1, 0),
+                    };
+                    inverse.TransformPoints(pixel_pts);
+                    dx = pixel_pts[1].X - pixel_pts[0].X;
+
+                    // Sine.
+                    List<PointF> sine_points = new List<PointF>();
+                    for (float x = (float)xmin; x <= xmax; x += dx)
+                    {
+                        sine_points.Add(new PointF(x, (float)Math.Sin(x)));
+                    }
+                    thin_pen.Color = Color.Red;
+                    gr.DrawLines(thin_pen, sine_points.ToArray());
+
+                    // Cosine.
+                    List<PointF> cosine_points = new List<PointF>();
+                    for (float x = (float)xmin; x <= xmax; x += dx)
+                    {
+                        cosine_points.Add(new PointF(x, (float)Math.Cos(x)));
+                    }
+                    thin_pen.Color = Color.Green;
+                    gr.DrawLines(thin_pen, cosine_points.ToArray());
+
+                    // Tangent.
+                    List<PointF> tangent_points = new List<PointF>();
+                    double old_value = Math.Tan(xmin);
+                    thin_pen.Color = Color.Blue;
+                    for (float x = (float)xmin; x <= xmax; x += dx)
+                    {
+                        // See if we're at a discontinuity.
+                        double new_value = Math.Tan(x);
+                        if ((Math.Abs(new_value - old_value) > 10) &&
+                            (Math.Sign(new_value) != Math.Sign(old_value)))
+                        {
+                            if (tangent_points.Count > 1)
+                                gr.DrawLines(thin_pen, tangent_points.ToArray());
+                            tangent_points = new List<PointF>();
+                        }
+                        else
+                        {
+                            tangent_points.Add(new PointF(x, (float)Math.Tan(x)));
+                        }
+                    }
+                    if (tangent_points.Count > 1)
+                        gr.DrawLines(thin_pen, tangent_points.ToArray());
+                }
+            }
+
+            // Display the result.
+            picGraph.Image = GraphImage;
+        }
+    }
+}
